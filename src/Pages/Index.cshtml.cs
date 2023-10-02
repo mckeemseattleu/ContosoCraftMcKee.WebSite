19a89212558f5ba13cb5650ee0870325466c6b6d
mckeemseattleu <mckeem@seattleu.edu>
2023-10-02 12:11:29 -0700

    Changed Index.cshtml.cs to reflect Mike's title

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Mike Koenig
+    /// Professor Mike Koenig
     /// </summary>
     public class IndexModel : PageModel
     {
