ff181e0e828558823b0aa790033a4733ebd891c1
mckeemseattleu <mckeem@seattleu.edu>
2023-10-02 16:05:25 -0700

    Added to 29

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Professor Mike Koenig December 28
+    /// Professor Mike Koenig December 29
     /// </summary>
     public class IndexModel : PageModel
     {
