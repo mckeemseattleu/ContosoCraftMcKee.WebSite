f526487ef72a849c74ec53943213be86158dd780
mckeemseattleu <mckeem@seattleu.edu>
2023-10-02 15:47:13 -0700

    Changed to 1973

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Professor Mike Koenig December
+    /// Professor Mike Koenig December 1973
     /// </summary>
     public class IndexModel : PageModel
     {
