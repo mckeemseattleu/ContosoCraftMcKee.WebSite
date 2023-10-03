4e237d5be7d1c908d7d6fc145c79119327d24dc7
mckeemseattleu <mckeem@seattleu.edu>
2023-10-02 18:36:30 -0700

    Changed to 2003

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,11 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
+<<<<<<< Updated upstream
     /// Professor Mike Koenig December 29 1993
+=======
+    /// Professor Mike Koenig December 29 2003
+>>>>>>> Stashed changes
     /// </summary>
     public class IndexModel : PageModel
     {
